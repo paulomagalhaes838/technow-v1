a80dc825e73bd72a064d8c604ef6514d82c3d9e9
paulomagalhaes838 <paulosergioti511@gmail.com>
2025-08-02 14:38:00 -0300

    Create init-firebase.js

---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+// Substitua pelos dados do seu projeto Firebase
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  databaseURL: "SUA_DATABASE_URL",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const auth = firebase.auth();
